bc5d727d05223d683f02ad4274bc72e545ffd941
danpatrickoneill <danpatrickoneill@gmail.com>
2020-07-23 20:57:45 -0400

    Added local storage persistence functions

---
+++ index.js
@@ -0,0 +1,18 @@
+// Full spec-compliant TodoMVC with localStorage persistence
+// and hash-based routing in ~120 effective lines of JavaScript.
+
+// Setup for local storage persistence
+const STORAGE_KEY = 'todos-vusje-2.0';
+const todoStorage = {
+  fetch: function () {
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    todos.forEach((todo, index) => {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  },
+};
