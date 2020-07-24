dc0091688e9c45657e6e6d66b95a8b4015851deb
danpatrickoneill <danpatrickoneill@gmail.com>
2020-07-24 19:19:55 -0400

    Added computed props and filter

--- index.js
+++ index.js
@@ -43,4 +43,28 @@ const app = new Vue({
       deep: true,
     },
   },
+
+  // Computed properties
+  computed: {
+    filteredTodos: function () {
+      return filters[this.visibility](this.todos);
+    },
+    remaining: function () {
+      return filters.active(this.todos).length;
+    },
+    allDone: {
+      get: function () {
+        return this.remaining === 0;
+      },
+      set: function (value) {
+        this.todos.forEach((todo) => {
+          todo.completed = value;
+        });
+      },
+    },
+  },
+
+  filters: {
+    pluralize: (n) => (n === 1 ? 'item' : 'items'),
+  },
 });
